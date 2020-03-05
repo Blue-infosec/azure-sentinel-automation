63ab3196f6c4050c383eb450c093b11b477d7f3e
Mikko Koivunen <61493860+mikoiv@users.noreply.github.com>
2020-03-05 16:51:56 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
-# Make sure you get Azure Subscription, Client and Tenant IDs from either Terraform or system variables. 
+# This has been tested with Terraform v0.12.21 (Azure Cloud Shell binaries)
+# Make sure you get Azure Subscription, Client and Tenant IDs from either Terraform or system variables
 provider "azurerm" {
   features {}
 }
